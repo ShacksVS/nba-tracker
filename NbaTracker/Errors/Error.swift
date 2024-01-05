1e118d4609c56356dcad1ec634ef3d098ed30515
ShacksVS <inode16@gmail.com>
2024-01-05 23:31:19 +0200

    added team struct and collected data from api

---
+++ NbaTracker/Errors/Error.swift
@@ -0,0 +1,8 @@
+//
+//  Error.swift
+//  NbaTracker
+//
+//  Created by Viktor Sovyak on 1/5/24.
+//
+
+import Foundation
---
+++ NbaTracker/Model/Team.swift
@@ -0,0 +1,8 @@
+//
+//  Team.swift
+//  NbaTracker
+//
+//  Created by Viktor Sovyak on 1/5/24.
+//
+
+import Foundation
