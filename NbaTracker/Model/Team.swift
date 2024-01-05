e4d86bb3491ccfdfdaded3974f1252122c067256
ShacksVS <inode16@gmail.com>
2024-01-06 00:02:15 +0200

    fixed team struct

--- NbaTracker/Model/Team.swift
+++ NbaTracker/Model/Team.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Team: Codable, Identifiable {
+    var id: Int
+    var abbreviation: String
+    var city: String
+    var conference: String
+    var division: String
+    var fullName: String
+    var name: String
+}
