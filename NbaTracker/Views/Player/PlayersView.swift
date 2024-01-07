5483f60bc7ccaa82abf54ea5ef0ff7b04ddce221
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-01-07 22:51:01 +0200

    Create a search for players and change the font color

--- NbaTracker/Views/Player/PlayersView.swift
+++ NbaTracker/Views/Player/PlayersView.swift
@@ -9,44 +9,59 @@ import SwiftUI
 
 struct PlayersView: View {
     @Environment(ModelData.self) var modelData
+    @State private var searchText: String = ""
+    
+    var filteredPlayers: [Player] {
+        guard !searchText.isEmpty else { return modelData.players }
+        return modelData.players.filter { $0.firstName.lowercased().contains(searchText.lowercased()) || $0.lastName.lowercased().contains(searchText.lowercased()) }
+    }
     
     var body: some View {
         NavigationSplitView {
-            ScrollView {
-                ForEach(modelData.players) { player in
-                    NavigationLink {
-                        PlayerDetailView(player: player)
-                    } label: {
-                        PlayerRow(player: player)
-                            .foregroundColor(.black)
+            if modelData.players.isEmpty {
+                ContentUnavailableView("No Players", systemImage: "person.2.slash",
+                                       description: Text("You need to have players here"))
+            } else {
+                if filteredPlayers.isEmpty {
+                    ContentUnavailableView.search(text: searchText)
+                } else {
+                    ScrollView {
+                        ForEach(filteredPlayers) { player in
+                            NavigationLink {
+                                PlayerDetailView(player: player)
+                            } label: {
+                                PlayerRow(player: player)
+                                    .foregroundColor(Color("TextColor"))
+                            }
+                        }
                     }
-                    
                 }
             }
-        } detail: {
-            Text("Players List")
         }
-        .padding()
-        .task {
-            do {
-                modelData.players = try await getAllPlayers()
-            } catch let error as ApiError {
-                switch(error){
-                case .invalidUrl:
-                    print("Invalid URL")
-                    
-                case .invalidResponse:
-                    print("Invalid response")
-                    
-                case .invalidData:
-                    print("Invalid data")
-                }
-            } catch {
-                print("Unexpected error: \(error)")
+    detail: {
+        Text("Players List")
+    }
+    .searchable(text: $searchText, prompt: "Search players")
+    .padding()
+    .task {
+        do {
+            modelData.players = try await getAllPlayers()
+        } catch let error as ApiError {
+            switch(error){
+            case .invalidUrl:
+                print("Invalid URL")
+                
+            case .invalidResponse:
+                print("Invalid response")
+                
+            case .invalidData:
+                print("Invalid data")
             }
+        } catch {
+            print("Unexpected error: \(error)")
         }
     }
-
+    }
 }
 
 #Preview {
