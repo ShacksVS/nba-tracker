d178d0566b8d6c49fd42e868f648195abc869c75
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-01-08 18:47:44 +0200

    Make attributes constant

--- NbaTracker/Model/Player.swift
+++ NbaTracker/Model/Player.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct Player: Codable, Identifiable {
-    var id: Int
-    var firstName: String
-    var lastName: String
-    var position: String
-    var team: Team
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let position: String
+    let team: Team
     
     static func mock(id:Int = 1, firstName: String = "Viktor", lastName: String = "Sovyak", position: String = "G", team: Team = Team.mock()) -> Player {
         return Player(id: id, firstName: firstName, lastName: lastName, position: position, team: team)
--- NbaTracker/Model/Team.swift
+++ NbaTracker/Model/Team.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct Team: Codable, Identifiable {
-    var id: Int
-    var abbreviation: String
-    var name: String
-    var fullName: String
-    var city: String
-    var conference: String
-    var division: String
+    let id: Int
+    let abbreviation: String
+    let name: String
+    let fullName: String
+    let city: String
+    let conference: String
+    let division: String
     
     static func mock(id: Int = 1, abbreviation: String = "Team Abbreviation", name: String = "Team Name", fullName: String = "Team Full Name", city: String = "Team City", conference: String = "Team Conference", division: String = "Team Division") -> Team {
         return Team(id: id, abbreviation: abbreviation, name: name, fullName: fullName, city: city, conference: conference, division: division)
